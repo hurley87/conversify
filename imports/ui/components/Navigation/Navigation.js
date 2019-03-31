918265d4191fa2c3da51ff9f845046fe5e1c2937
david hurley <dhurls99@gmail.com>
2019-03-31 16:36:28 -0400

    awesome

--- imports/ui/components/Navigation/Navigation.js
+++ imports/ui/components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ const Navigation = props => (
   <Navbar>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to="/">Gatsbie</Link>
+        <Link to="/">ICM HUB</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
