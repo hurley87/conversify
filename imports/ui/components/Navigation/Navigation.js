27726b50f6b8a017e1f85da5cd5ae37cade5915c
david hurley <dhurls99@gmail.com>
2019-04-04 21:57:05 -0400

    ionital

--- imports/ui/components/Navigation/Navigation.js
+++ imports/ui/components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ const Navigation = props => (
   <Navbar>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to="/">ICM HUB</Link>
+        <Link to="/">Client Magnet</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
