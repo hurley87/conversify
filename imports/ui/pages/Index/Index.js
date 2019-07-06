62659ec092cf1b2d7421c7d3d7a10623c71e2993
david hurley <dhurls99@gmail.com>
2019-07-06 17:03:01 -0400

    sweet

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -7,104 +7,111 @@ import './Index.scss';
 const Index = () => (
   <div className="Index">
     <div className="hero-container">
-      <div className='container'>
-        <Row>
-          <Col xs={12} sm={6}>
-            <h1>
-            Your all-in-one growth marketing platform.
-            </h1>
-            <p>
-            Conversify is a complete Email, LinkedIn and Live Chat solution designed to help you manage all your sales conversations in one place.
-            </p>
-            <br />
-            <p><a href="/signup">Get Started For Free</a></p>
-          </Col>
-          <Col xs={12} sm={6}>
-            <img src="Tips.png"/>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <div className='header'>
-              <h3>
-                Direct selling doesn’t work like it used to
-              </h3>
-              <p>Surround your prospect with value and have them come to you</p>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={4} xs={12}>
-            <div className='step'>
-              <img src="step_1.png"/>
-              <p>You approve a list of qualified prospects and a sequence of messages</p>
-            </div>
-          </Col>
-          <Col sm={4} xs={12}>
-            <div className='step'>
-              <img src="step_2.png"/>
-              <p>We send a sequence of messages over LinkedIn on your behalf</p>
-            </div>
-          </Col>
-          <Col sm={4} xs={12}>
-            <div className='step'>
-              <img src="step_3.png"/>
-              <p>We email prospects based on how they responded over LinkedIn</p>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <div className='header text-center'>
-              <h3>
-                Put your prospecting on autopilot
-              </h3>
-              <p>Without having to spend money on ads or any other complicated marketing funnel</p>
-              <br/>
-              <p><a style={{margin: 'auto'}} href="/signup">Get Started For Free</a></p>
-            </div>
-          </Col>
-        </Row>
+      <div className='rectangle'>
+        <div className='container'>
+          <Row>
+            <Col xs={12} sm={6}>
+              <h1>
+              Your all-in-one B2B growth marketing platform
+              </h1>
+              <p>
+              Created to help you allow your sales team to be more productive. 
+              </p>
+              <br />
+              <p><a href="/signup">Get Started</a></p>
+            </Col>
+          </Row>
+        </div>
       </div>
     </div>
- 
-{/* <div className='first'>
-      <div className='text container'>
-            <h3>
-              Templates
-            </h3>
-            <p>
-              Surround is the best way to reach our ideal customers. 
-              We help you sychronize the distribution of value over email and LinkedIn in a way that delights your prospects.
-            </p>
-      </div>
-    </div>
-    <div className='second'>
-      <div className="container">
-      <Row>
+    <div className='content-container container'>
+        <Row>
+          <Col xs={12}>
+            <h3>All the growth tools you need</h3>
+            <div className='line'></div>
+          </Col>
+        </Row>
+        <Row className='section'>
+          <Col xs={6}>
+            <h4>Prospects</h4>
+            <h2>Get started quickly</h2>
+            <p>Created the perfect list of contact to go after.</p>
+          </Col>
+          <Col xs={6}>
+            <p className='text-right'><img src="3.png"/></p>
+          </Col>
+        </Row>
+        <Row className='section'>
         <Col xs={6}>
-          <h3>
-            Prospects
-          </h3>
-          <p>
-            Surround is the best way to reach our ideal customers. 
-            We help you sychronize the distribution of value over email and LinkedIn in a way that delights your prospects.
-          </p>
-        </Col>
-      </Row>
+            <img src="1.png"/>
+          </Col>
+          <Col xs={6} className='text-right'>
+            <h4>Templates</h4>
+            <h2>Get started quickly</h2>
+            <p>Created the perfect list of contact to go after.</p>
+          </Col>
+        </Row>
+        <Row className='section'>
+          <Col xs={6}>
+            <h4>Outreach</h4>
+            <h2>Get started quickly</h2>
+            <p>Created the perfect list of contact to go after.</p>
+          </Col>
+          <Col xs={6}>
+          <p className='text-right'><img src="2.png"/></p>
+          </Col>
+        </Row>
+        <Row className='section'>
+        <Col xs={6}>
+            <img src="5.png"/>
+          </Col>
+          <Col xs={6} className='text-right'>
+            <h4>Reporting</h4>
+            <h2>Get started quickly</h2>
+            <p>Created the perfect list of contact to go after.</p>
+          </Col>
+        </Row>
+        <Row className='section'>
+          <Col xs={12}>
+            <h3>Put your prospecting on autopilot</h3>
+            <p className='text-center'>This is an amazing concluding statement</p>
+          </Col>
+          <Col smOffset={4} sm={4}>
+            <p className='text-center'><a className='add-btn' href="/signup">Get Started</a></p>
+          </Col>
+        </Row>
       </div>
-    </div>
-    <div className='first'>
-      <div className='text container'>
-            <h3>
-              Sales Funnel
-            </h3>
-            <p>
-              Surround is the best way to reach our ideal customers. 
-              We help you sychronize the distribution of value over email and LinkedIn in a way that delights your prospects.
-            </p>
+      <div className='footer'>
+        <div className='container'>
+          <Row>
+            <Col xs={3}>
+              <img height='25px' src="https://adsgen.s3.amazonaws.com/conversify-logo.svg"/>
+            </Col>
+            <Col xs={3}>
+              <h4>Company</h4>
+              <p><a href="/about">About</a></p>
+              <p><a href="/about">Pricing</a></p>
+              <p><a href="/about">Careers</a></p>
+            </Col>
+            <Col xs={3}>
+              <h4>Product</h4>
+              <p><a href="/about">List Building</a></p>
+              <p><a href="/about">Advanced Sequences</a></p>
+              <p><a href="/about">LinkedIn Automation</a></p>
+              <p><a href="/about">Email Automation</a></p>
+              <p><a href="/about">Live Chat</a></p>
+            </Col>
+            <Col xs={3}>
+              <h4>Contact</h4>
+              <p>+1 (800) 651-9632</p>
+              <p>support@conversify.ai</p>
+              <p>Request a demo</p>
+              <p>Site Map</p>
+            </Col>
+          </Row>
+        </div>
+
       </div>
-    </div> */}
   </div>
 );
 
