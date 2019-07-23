2534fe24cbd57d1b7a28b888f7d7f3fd4e3537c3
david hurley <dhurls99@gmail.com>
2019-07-23 12:07:37 -0400

    awesome

--- imports/ui/components/Footer/Footer.js
+++ imports/ui/components/Footer/Footer.js
@@ -12,51 +12,89 @@ const copyrightYear = () => {
 
 const Footer = () => (
   <div className='Footer'>
-    <div className="get-started">
-      <div className='container text-center'>
-      <Row>
-        <Col md={4} sm={12} mdOffset={4} xs={12}>
+    <div className='container'>
+    <br />
+    <br />
+    <br />
+    <br />
+    <Row>
+      <Col md={8} sm={12} mdOffset={2} xs={12}>
         <h1>Get started</h1>
         <p>Conversify offers services that make it easy for sales leaders to reach their growth goals.</p>
+        <br />
         <p><a className='add-btn' href='https://calendly.com/conversifyai'>Get a Demo</a></p>
-        </Col>
-      </Row>
-      </div>
-    </div>
-    <div className='container'>
-    <Row>
-      <Col sm={3}>
-        <img height='25px' src="https://adsgen.s3.amazonaws.com/conversify-logo.svg"/>
-      </Col>
-      <Col sm={3}>
-        <h4>Company</h4>
-        <p><a href="/about">About</a></p>
-        <p><a href="/golf">Golf</a></p>
-        <p><a href="/terms">Terms</a></p>
-        <p><a href="/privacy">Privacy</a></p>
-      </Col>
-      <Col sm={3}>
-        <h4>Product</h4>
-        <p><a href="/prospecting">Prospecting</a></p>
-        <p><a href="/messaging">Messaging</a></p>
-        <p><a href="/automation">Automation</a></p>
-        <p><a href="/reporting">Reporting</a></p>
-      </Col>
-      <Col sm={3}>
-        <h4>Contact</h4>
-        <p>+1 (647) 284-5023</p>
-        <p>david@conversify.ai</p>
-        <p><a href="https://calendly.com/conversifyai">Get a Demo</a></p>
-      </Col>
-      <Col xs={12}>
-        <br />
-        <br />
-        <br />
-        <br />
-        <p className='text-center'><small>Copyright Conversify Inc.</small></p>
       </Col>
     </Row>
+    <br />
+    <br />
+    <br />
+    <br />
+    <hr />
+    <br />
+    <br />
+    <Row>
+      <Col sm={4}>
+        <p className='text-left'>Copyright Conversify Inc.</p>
+      </Col>
+      <Col sm={4}>
+        <img height='25px' src="https://adsgen.s3.amazonaws.com/conversify-logo.svg"/>
+      </Col>
+      <Col sm={4}>
+        <p className='text-right'><a href="/terms">Terms</a> &nbsp;&nbsp;&nbsp;<a href="/privacy">Privacy</a></p>
+      </Col>
+    </Row>
+    <br />
+    <br />
   </div>
+{
+
+  //   <div className="get-started">
+  //     <div className='container text-center'>
+  //     <Row>
+  //       <Col md={4} sm={12} mdOffset={4} xs={12}>
+  //       <h1>Get started</h1>
+  //       <p>Conversify offers services that make it easy for sales leaders to reach their growth goals.</p>
+  //       <p><a className='add-btn' href='https://calendly.com/conversifyai'>Get a Demo</a></p>
+  //       </Col>
+  //     </Row>
+  //     </div>
+  //   </div>
+  //   <div className='container'>
+  //   <Row>
+  //     <Col sm={3}>
+  //       <img height='25px' src="https://adsgen.s3.amazonaws.com/conversify-logo.svg"/>
+  //     </Col>
+  //     <Col sm={3}>
+  //       <h4>Company</h4>
+  //       <p><a href="/about">About</a></p>
+  //       <p><a href="/golf">Golf</a></p>
+  //       <p><a href="/terms">Terms</a></p>
+  //       <p><a href="/privacy">Privacy</a></p>
+  //     </Col>
+  //     <Col sm={3}>
+  //       <h4>Product</h4>
+  //       <p><a href="/prospecting">Prospecting</a></p>
+  //       <p><a href="/messaging">Messaging</a></p>
+  //       <p><a href="/automation">Automation</a></p>
+  //       <p><a href="/reporting">Reporting</a></p>
+  //     </Col>
+  //     <Col sm={3}>
+  //       <h4>Contact</h4>
+  //       <p>+1 (647) 284-5023</p>
+  //       <p>david@conversify.ai</p>
+  //       <p><a href="https://calendly.com/conversifyai">Get a Demo</a></p>
+  //     </Col>
+  //     <Col xs={12}>
+  //       <br />
+  //       <br />
+  //       <br />
+  //       <br />
+  //       <p className='text-center'><small>Copyright Conversify Inc.</small></p>
+  //     </Col>
+  //   </Row>
+  // </div>
+
+  }
 </div>
 );
 
--- imports/ui/components/PublicNavigation/PublicNavigation.js
+++ imports/ui/components/PublicNavigation/PublicNavigation.js
@@ -4,12 +4,16 @@ import { Nav, NavItem } from 'react-bootstrap';
 
 const PublicNavigation = () => (
   <Nav pullRight>
-    <LinkContainer to="/about">
-      <NavItem eventKey={1} href="/about">About</NavItem>
-    </LinkContainer>
-    <LinkContainer to="/golf">
-      <NavItem eventKey={2} href="/golf">Golf</NavItem>
-    </LinkContainer>
+{
+
+    // <LinkContainer to="/about">
+    //   <NavItem eventKey={1} href="/about">About</NavItem>
+    // </LinkContainer>
+    // <LinkContainer to="/golf">
+    //   <NavItem eventKey={2} href="/golf">Golf</NavItem>
+    // </LinkContainer>
+
+    }
     <LinkContainer to="/login">
       <NavItem eventKey={3} href="/login">Log In</NavItem>
     </LinkContainer>
